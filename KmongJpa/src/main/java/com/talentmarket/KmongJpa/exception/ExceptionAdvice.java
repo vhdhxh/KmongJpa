7d2421928e2c37950d062e6e97b0781d01bf5458
dev yoon <vhdhxh@naver.com>
2024-02-04 19:37:03 +0900

    apiResponse 추가

--- KmongJpa/src/main/java/com/talentmarket/KmongJpa/exception/ExceptionAdvice.java
+++ KmongJpa/src/main/java/com/talentmarket/KmongJpa/exception/ExceptionAdvice.java
@@ -34,7 +34,6 @@ public class ExceptionAdvice {
         log.info(exception.getFieldError().getDefaultMessage());
 
 
-
         return ApiResponse.of(HttpStatus.BAD_REQUEST, exception.getBindingResult().getAllErrors().get(0).getDefaultMessage(),null);
 
 //        System.out.println("==========================");
