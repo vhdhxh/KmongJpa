4303bdf4f41517529df8b2ef83c1d225da764c2b
dev yoon <vhdhxh@naver.com>
2024-03-22 23:52:14 +0900

    주문 비지니스로직 리팩토링

--- KmongJpa/src/main/java/com/talentmarket/KmongJpa/repository/ItemRepository.java
+++ KmongJpa/src/main/java/com/talentmarket/KmongJpa/repository/ItemRepository.java
@@ -17,6 +17,9 @@ public interface ItemRepository extends JpaRepository<Item,Long> {
     Optional<Item> findItemAndComment(@Param("Id") Long Id);
 
 
+    //이 쿼리의 문제점 : 이쿼리는 count 쿼리와 아이템 리스트를 불러오는 쿼리 두번의 쿼리가 나간다.
+    //count 쿼리는 데이터가 많아질수록 부담이 크다 .
+    //또한 페이지가 뒤로갈수록 응답속도가 느려진다.
     @Query("select I from Item I join fetch I.users")
     Page<Item> findItem(Pageable pageable);
     List<Item> findAllByIdIn(List<Long> itemIds);
