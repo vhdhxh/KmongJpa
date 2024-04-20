02d12a526136d91e99209c2a870dd1359e76aa25
dev yoon <vhdhxh@naver.com>
2024-04-20 21:39:45 +0900

    채팅비즈니스 로직 리팩토링

--- KmongJpa/src/main/java/com/talentmarket/KmongJpa/chat/application/ChatService.java
+++ KmongJpa/src/main/java/com/talentmarket/KmongJpa/chat/application/ChatService.java
@@ -17,6 +17,8 @@ import org.springframework.stereotype.Service;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.*;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -31,76 +33,49 @@ public class ChatService {
     //문의하기 버튼을 누르면, 상대와 나의 채팅 방이 있는지 확인하고, 없다면 만들어서 채팅방 id를 넘겨주고
     // 있다면 기존 채팅방 id를 넘겨준다.
     public Long createRoom(Users user, Long user2Id) {
-
-        Long chatRoomId;
         Users user1 = user;
         Long user1Id = user1.getId();
         Optional<ChatRoom> chatRoom = chatRoomRepository.findChatUser(user1Id, user2Id);
-        if (chatRoom.isEmpty()) {
-            Users user2 = userRepository.findById(user2Id).get();
-            ChatRoom chatRoom1 = ChatRoom.createRoom(user1, user2);
-            chatRoomId = chatRoomRepository.save(chatRoom1).getId();
-            return chatRoomId;
-        }
-        chatRoomId = chatRoom.get().getId();
+        Long chatRoomId = getRoomId(chatRoom,user2Id,user1);
         return chatRoomId;
-
-
-
     }
 
     public SendResponse sendMessage(SendRequest req, String nickName) {
         Long senderId = req.getSenderId();
         Long receiverId = req.getReceiverId();
         String message = req.getMessage();
-
         ChatRoom chatRoom = chatRoomRepository.findChatUser(senderId, receiverId)
-                .orElseThrow(() -> new CustomException(ErrorCode.CHATROOM_NOT_FOUND));
-           Users user = userRepository.findById(senderId).get();
-           String senderName = user.getName();
-           SendResponse sendResponse = new SendResponse(senderName,message);
+                            .orElseThrow(() -> new CustomException(ErrorCode.CHATROOM_NOT_FOUND));
+        Users user = userRepository.findById(senderId).get();
+        String senderName = user.getName();
+        SendResponse sendResponse = new SendResponse(senderName,message);
         Chat chat = Chat.createChat(chatRoom, message, senderId ,receiverId);
-
         chatRepository.save(chat);
         return sendResponse;
     }
 
     public List<ChatListResponse> getChatList(Users user) {
         Long userId = user.getId();
-        log.info(String.valueOf(userId));
-        //chatroomRepository 에서 유저의 모든 방 리스트 엔티티를 찾아온다.
         List<ChatRoom> chatRoomList = chatRoomRepository.findChatRoom(userId);
-        log.info(chatRoomList.toString());
-        //유저의 대화 상대 Id 리스트
         List<Long> partnerIdList = new ArrayList<>(); // 초기화 추가
-
-        for (ChatRoom chatRoom : chatRoomList) {
-            Long partnerId = chatRoom.getUser1().getId().equals(userId) ? chatRoom.getUser2().getId() : chatRoom.getUser1().getId();
-            partnerIdList.add(partnerId);
-        }
-
+        chatRoomList.stream().forEach(chatRoom -> addPartnerIdList(chatRoom,partnerIdList,userId));
         List<Users> users = userRepository.findAllById(partnerIdList);
-        Map<Long, String> userIdToNameMap =
-                users.stream().collect(Collectors.toMap(Users::getId, Users::getName));
-
-        List<Long> chatRoomIdList = chatRoomList.stream().map(ChatRoom::getId).collect(Collectors.toList());
-        log.info(chatRoomIdList.toString());
+        Map<Long, String> userIdToNameMap = users.stream().collect(toMap(Users::getId, Users::getName));
+        List<Long> chatRoomIdList = chatRoomList.stream().map(ChatRoom::getId).collect(toList());
         List<Chat> chats = chatRepository.findChatList(chatRoomIdList);
-      log.info(chats.toString());
-        List<ChatListResponse> chatListResponses = chats.stream().map(chat -> {
-            Long partnerId = chat.getSenderId().equals(userId) ? chat.getReceiverId() : chat.getSenderId();
-
-
-            String otherNickname = userIdToNameMap.get(partnerId);
-
-            return ChatListResponse.builder()
-                    .otherNickname(otherNickname)
-                    .recentMessage(chat.getMessage())
-                    .roomId(chat.getChatRoom().getId())
-                    .build();
-        }).toList();
-
+        List<ChatListResponse> chatListResponses = chats.stream().map(chat -> toChatListResponse(chat,userId,userIdToNameMap)).toList();
         return chatListResponses;
+//        priavte List<ChatListResponse> toChatListResponse(List<Chat> chats, Long userId , Map<Long, String> userIdToNameMap);
+//        List<ChatListResponse> chatListResponses = chats.stream().map(chat -> {
+//            Long partnerId = chat.getSenderId().equals(userId) ? chat.getReceiverId() : chat.getSenderId();
+//            String otherNickname = userIdToNameMap.get(partnerId);
+//            return ChatListResponse.builder()
+//                    .otherNickname(otherNickname)
+//                    .recentMessage(chat.getMessage())
+//                    .roomId(chat.getChatRoom().getId())
+//                    .build();
+//        }).toList();
+
     }
     public List<ChatResponse> getChat (Users user, ChatRequest req , Long roomId) {
         Long userId = user.getId();
@@ -113,19 +88,36 @@ public class ChatService {
         Map<Long, String> userIdToNameMap = new HashMap<>();
         userIdToNameMap.put(userId, userNickName);
         userIdToNameMap.put(partnerId,partnerName);
-
-         List<ChatResponse> chatResponses = chatList
-                 .stream()
-                 .map(c-> ChatResponse
-                              .builder()
+        return  chatList.stream().map(c-> ChatResponse.builder()
                               .roomId(roomId)
                               .senderNickname(userIdToNameMap.get(c.getSenderId()))
                               .receiverNickname(userIdToNameMap.get(c.getReceiverId()))
                               .message(c.getMessage())
                               .build()
-                 ).collect(Collectors.toList());
+                 ).collect(toList());
+    }
+    private Long getRoomId(Optional<ChatRoom> chatRoom, Long user2Id , Users user1) {
+        if (chatRoom.isEmpty()) {
+            Users user2 = userRepository.findById(user2Id).get();
+            ChatRoom chatRoom1 = ChatRoom.createRoom(user1, user2);
+            Long chatRoomId = chatRoomRepository.save(chatRoom1).getId();
+            return chatRoomId;
+        }
+        return chatRoom.get().getId();
+    }
+    private void addPartnerIdList(ChatRoom chatRoom,List<Long> partnerIdList,Long userId) {
+       Long partnerId = chatRoom.getUser1().getId().equals(userId) ? chatRoom.getUser2().getId() : chatRoom.getUser1().getId();
+       partnerIdList.add(partnerId);
+    }
 
-        return chatResponses;
+    private ChatListResponse toChatListResponse(Chat chat, Long userId , Map<Long, String> userIdToNameMap) {
+        Long partnerId = chat.getSenderId().equals(userId) ? chat.getReceiverId() : chat.getSenderId();
+        String otherNickname = userIdToNameMap.get(partnerId);
+        return ChatListResponse.builder()
+                .otherNickname(otherNickname)
+                .recentMessage(chat.getMessage())
+                .roomId(chat.getChatRoom().getId())
+                .build();
     }
 }
 
